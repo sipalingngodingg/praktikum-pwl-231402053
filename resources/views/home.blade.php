05c0aa284cd20dbe81c56de4edc44ea6ef18c46b
sipalingngodingg <prayersitepu@gmail.com>
2024-03-13 18:21:54 +0700

    tugas 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -67,10 +67,11 @@
   {{-- akhir task --}}
 </div>
 
-<footer class="footer footer-center p-4 bg-base-300 text-base-content">
-  <aside>
-    <p>Copyright © 2024 - All right reserved by Prayer Sitepu</p>
-  </aside>
-</footer>
+<footer class="footer footer-center p-4 bg-base-300 text-base-content bottom-0 absolute">
+    <aside>
+      <p>Copyright © 2024 - All right reserved by Prayer Sitepu</p>
+    </aside>
+  </footer>
+</div>
 {{-- akhir content --}}
 @endsection
--- resources/views/layouts/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer footer-center p-4 bg-base-300 text-base-content">
-  <aside>
-    <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
-  </aside>
-</footer>
